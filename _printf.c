5c244d4a7a951ec9a214b028213a8d8b3449d8d7
EngAmel <amel.abdululazim.se@gmail.com>
2023-10-14 09:43:03 -0700

    _printf function added

---
+++ _printf.c
@@ -0,0 +1,52 @@
+#include "main.h"
+/**
+ * _printf - function that print output
+ * @format:type of inputs
+ *
+ * Return: 0
+ **/
+
+int _printf(const char *format, ...)
+{
+	va_list args;
+	char buffer[1024];
+	int i = 0;
+	int buffer_index = 0;
+
+	va_start(args, format);
+
+	while (format != NULL && format[i] != '\0')
+	{
+		switch (format[i])
+		{
+			case 'c':
+				buffer[buffer_index++] = (char)va_arg(args, int);
+				break;
+			case 's':
+				{
+					char *str = va_arg(args, char *);
+
+					if (str == NULL)
+						str = "(nil)";
+					while (*str)
+						buffer[buffer_index++] = *str++;
+				}
+				break;
+		}
+		if ((format[i] == 'c' || format[i] == 's') && format[i + 1] != '\0')
+		{
+			buffer[buffer_index++] = ',';
+			buffer[buffer_index++] = ' ';
+		}
+		i++;
+	}
+
+	buffer[buffer_index] = '\0';
+
+	for (int j = 0; buffer[j] != '\0'; j++)
+		putchar(buffer[j]);
+	putchar('\n');
+	va_end(args);
+	return (0);
+}
+
